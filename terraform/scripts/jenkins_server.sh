cd096c42c3fbd3be8b7ca75fe2d41bddc215ba6d
SridharKidambi <sridharhere@gmail.com>
2019-12-16 18:54:07 +0530

    code changes

--- terraform/scripts/jenkins_server.sh
+++ terraform/scripts/jenkins_server.sh
@@ -108,10 +108,22 @@ function configure_jenkins_server ()
   cd $plugins_dir || { echo "unable to chdir to [$plugins_dir]"; exit 1; }
 
   # List of plugins that are needed to be installed 
-  plugin_list="git-client git github-api github-oauth github MSBuild ssh-slaves workflow-aggregator ws-cleanup pipeline-maven aws-java-sdk docker-plugin ocker-java-api"
+  plugin_list="git-client git github-api github-oauth github MSBuild ssh-slaves workflow-aggregator ws-cleanup pipeline-maven aws-java-sdk docker-plugin docker-java-api chromedriver shiningpanda python"
 
   # remove existing plugins, if any ...
   rm -rfv $plugin_list
+
+#selenium needs
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+
+sudo apt-get install google-chrome-stable
+pip install behave
+pip install selenium
+pip install virtualenv
+
 
   for plugin in $plugin_list; do
       echo "installing plugin [$plugin] ..."
